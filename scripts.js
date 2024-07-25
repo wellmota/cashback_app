b4dac5ba8a4800f407e06b304e9cb2e71dafd056
wellmota <impeto.wellington@gmail.com>
2024-07-25 19:02:02 -0300

    chore: Refactor expense form submission logic

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 //Select form elements
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Allow only numbers
 amount.oninput = () => {
@@ -21,4 +24,21 @@ function formatCurrencyBRL(value) {
   })
 
   return value
+}
+
+// Submit form
+form.onsubmit = (event) => {
+  // Prevent the form from submitting
+  event.preventDefault()
+  // Create a new expense object
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+
+  console.log(newExpense)
 }
