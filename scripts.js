8ff2a66af159f772ec87bc926583ae6f1912d32e
wellmota <impeto.wellington@gmail.com>
2024-07-25 19:34:10 -0300

    chore: Add new expense to the list when submitted

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
+// Load categories
+const expenseList = document.querySelector("ul")
+
 // Allow only numbers
 amount.oninput = () => {
   // Remove any non-digit character
@@ -39,6 +42,46 @@ form.onsubmit = (event) => {
     amount: amount.value,
     created_at: new Date(),
   }
+  // Add the new expense to the list
+  expenseAdd(newExpense)
+}
 
-  console.log(newExpense)
+// Add new expense function
+function expenseAdd(newExpense) {
+  try {
+    // Create a new list item
+    const expenseItem = document.createElement("li")
+    expenseItem.classList.add("expense")
+
+    // Add the expense item to the list
+    const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+
+    // Add the expense icon to the list
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    // Create expense div
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    // Add the expense category
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    // Add the expense name
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    // Add name and category to expense div
+    expenseInfo.append(expenseName, expenseCategory)
+
+    // Add the expense icon to the list
+    expenseItem.append(expenseIcon, expenseInfo)
+
+    // Create a new div element
+    expenseList.append(expenseItem)
+  } catch (error) {
+    alert("Erro ao adicionar despesa")
+    console.log(error)
+  }
 }
